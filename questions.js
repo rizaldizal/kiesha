a369b23792984c3e5e570776b026fe864c1543cf
rizaldizal <139616406+rizaldizal@users.noreply.github.com>
2023-10-27 04:16:38 +0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,24 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    question: "Makasih selalu ada buat aku sampai saat ini, maaff kalo izal belum bisa ngasih yang terbaik buat eca tetapi izal bakalan berusaha jadi versi yang terbaik yang eca mau Makasih juga udah mau nerima kekurangan izal, makasih juga udah mau nerima izal yang suka ninggiin gengsi sama ego. pokonya aku SUPER SUPER MAKASIH BUAT KAMU I LOVE YOUU",
+    answer: "lanjut",
+    options: [
+      "lanjut"
+    ]
+  },
+  {
+    question: "Kalimat cinta yang disusun dengan baik dengan kata-kata manis tidak dapat benar-benar mengungkapkan betapa berartinya dirimu bagiku dan melukiskan bagaimana cintamu yang istimewa membuat aku merasa makin bahagia setiap hari. Dan jika hatiku adalah sebuah rumah, fondasi yang kokoh di dalam hatiku akan dibangun dengan perhatian dan cintamu. Izal akan selau mencintai eca. I LOOVE YOUU.",
+    answer: "lanjut",
+    options: [
+      "lanjut"
+    ]
+  },
+  {
+  question: "Ketika ditanya apa yang kubutuhkan dalam hidup, inilah jawabanku. Yang kubutuhkan dan kuinginkan hanyalah Allah Umi Apa kamu dan restu orang tua mu asekkk. Semua hal akan memudar seiring berjalannya waktu, tapi kamu akan terus menguat dan tumbuh makin dalam dihatiku seiring berjalannya waktu. Banyak hal yang tidak akan selalu diingat, tetapi cinta manismu akan selalu terkenang. Aku mencintaimu lebih dari yang kamu tahu, I LOVE YOUU",
+  answer: "Lanjut",
+  options: [
+  "Lanjut",
+  ]
+}
+];              
